813186099ae265b17746c2f5b7381a22d05c6b6e
michalz98 <62769658+michalz98@users.noreply.github.com>
2021-04-26 17:18:25 +0200

    Add files via upload

--- obraz.h
+++ obraz.h
@@ -11,7 +11,7 @@ public:
     void dodajPiksel(int x, int  y);                                //rysowanie pojedynczego piksela
     void rysujLinie(int x1, int y1, int x2, int y2);                //rysowanie dowolnej linii
     void rysujProstokat(int x1, int y1, int x2, int y2, bool wypel);  //rysowanie prostokata rownoleglego do osi, wypel=0 - bez wypelniania, wypel=1 - z wypelnieniem
-//  void rysujOkrag(int x, int y, int r);
+    void rysujOkrag(int x, int y, int r);
     void wyczysc();                                                 //czyszczenie calego obrazu
 };
 
