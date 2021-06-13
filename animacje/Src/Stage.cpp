ec75b2662c162ee07d2e3157b76120d00e7b3bdd
Kacper Grabowski <kapi2700@tlen.pl>
2021-06-13 16:37:06 +0200

    komunikacja v0.1

--- animacje/Src/Stage.cpp
+++ animacje/Src/Stage.cpp
@@ -16,3 +16,18 @@ bool Stage::clear(){
 		}
 	}
 }
+
+void Stage::comms()
+{
+	for(int i=0; i<240; i++
+	{
+		for(int j =0; j<320; j++)
+		{
+			if(p[i][j])
+			{
+				printf();
+			}
+		}
+	}
+	printf("k\r\n");
+}
