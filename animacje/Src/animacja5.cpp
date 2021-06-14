6b0d28399bef4e789dfc5efc70091d1c4fb412d2
michalz98 <62769658+michalz98@users.noreply.github.com>
2021-06-14 16:35:42 +0200

    Add files via upload

---
+++ animacje/Src/animacja5.cpp
@@ -0,0 +1,65 @@
+/*
+ * animacja5.cpp
+ *
+ *  Created on: 14 cze 2021
+ *      Author: michal
+ */
+
+#include "animacja5.h"
+
+void circles(int step)
+{
+	int x = 120, y= 160;
+
+	if(step%30 = 0 && stgobj->rw1 == 0){
+		stgobj->rw1 == 1;
+	}
+	if(step%60 = 0 && stgobj->rw1 == 0){
+		stgobj->rw2 == 1;
+	}
+	if(step%90 = 0 && stgobj->rw1 == 0){
+		stgobj->rw3 == 1;
+	}
+	if(step%120 = 0 && stgobj->rw1 == 0){
+		stgobj->rw4 == 1;
+	}
+
+	stgobj->wave1->drawCircle(x, y);
+	stgobj->wave2->drawCircle(x, y);
+	stgobj->wave3->drawCircle(x, y);
+	stgobj->wave4->drawCircle(x, y);
+
+
+	if( stgobj->rw1 != 0){
+		stgobj->rw1 += step;
+	}
+
+	if( stgobj->rw1 != 0){
+		stgobj->rw1 += step;
+	}
+
+	if( stgobj->rw1 != 0){
+		stgobj->rw1 += step;
+	}
+
+	if( stgobj->rw1 != 0){
+		stgobj->rw1 += step;
+	}
+
+	if( stgobj->rw1 > 115){
+		stgobj->rw1 = 0;
+	}
+
+	if( stgobj->rw2 > 115){
+		stgobj->rw2 = 0;
+	}
+
+	if( stgobj->rw3 > 115){
+		stgobj->rw3 = 0;
+	}
+
+	if( stgobj->rw4 > 115){
+		stgobj->rw4 = 0;
+	}
+}
+
