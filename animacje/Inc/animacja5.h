e950cd187098287d9f99ce6b297409c7f0e92740
michalz98 <62769658+michalz98@users.noreply.github.com>
2021-06-14 16:36:06 +0200

    Add files via upload

---
+++ animacje/Inc/animacja5.h
@@ -0,0 +1,18 @@
+/*
+ * animacja5.h
+ *
+ *  Created on: 14 cze 2021
+ *      Author: michal
+ */
+
+#ifndef INC_ANIMACJA5_H_
+#define INC_ANIMACJA5_H_
+
+#include "Stage.h"
+#include "StageObj.h"
+
+void circles(int step);
+
+
+
+#endif /* INC_ANIMACJA5_H_ */
