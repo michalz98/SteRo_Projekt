901d789d443ab55274cf0043d4dcd3035509ada1
michalz98 <62769658+michalz98@users.noreply.github.com>
2021-06-14 16:36:29 +0200

    Add files via upload

--- animacje/Inc/StageObj.h
+++ animacje/Inc/StageObj.h
@@ -17,20 +17,24 @@ public:
 	int r1, r2, rb, w1, w2, l1, l2, vxb, vyb;
 	int x1, x2, x3, x4, xb;
 	int y1,y2 ,y3 ,y4, yb;
+	int rw1, rw2, rw3, rw4;
 	std::vector<int> x_axis;
 	std::vector<int> y_axis;
 	Circle *cir1;
 	Circle *cir2;
 	Circle *ball;
+	Circle *wave1;
+	Circle *wave2;
+	Circle *wave3;
+	Circle *wave4;
 	Rectangle *rec1;
 	Rectangle *rec2;
-private:
 	StageObj();
 	~StageObj();
 	bool initFig();
 
 };
 
-StageObj *stgobj;
+static StageObj *stgobj;
 
 #endif /* INC_STAGEOBJ_H_ */
